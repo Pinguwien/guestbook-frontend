01d6e6d5cfb236ae6bc9b7b5efbe10f59a830a90
Dominik Guhr <dominik.guhr@codecentric.de>
2017-09-04 11:25:15 +0200

    changed ajaxcall to post

--- src/main/resources/static/js/add.js
+++ src/main/resources/static/js/add.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 		
 		$.ajax({
 			url:"http://127.0.0.1:8090/guestbook",
-			method:"PUT",
+			method:"POST",
             headers: {
                 "Authorization": "Bearer " + keycloak.token
             },
